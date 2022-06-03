ea71d374ea08ad3a08e59bd1be1fe610922e1c99
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-06-03 15:55:34 +0200

    TestAPIPage(Edit)

--- pages/test-api.js
+++ pages/test-api.js
@@ -1,4 +1,28 @@
+import { useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+
+import { TestingResultsContext } from '../context/TestingResultsContext';
+
 const TestApiPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const testingResultsCtx = useContext(TestingResultsContext);
+
+  const router = useRouter();
+
+  const submitFormHandler = async (formData) => {
+    // setLoading = true
+    // fetch post request => http://localhost:3000/testapi
+    // wait for response
+    
+    // set response data to context => setResultsData(response.data)
+    // else set Error
+
+    // setLoading false
+    // redirect to results => router.replace
+  };
+
   return <h1> Test Your API </h1>;
 };
 
