1e3217fcc4a6588b6750c5582be4edb4faac722d
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-06-03 15:41:24 +0200

    TestAPIPage

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,29 +1,40 @@
 import Link from 'next/link';
+import { useContext } from 'react';
+
+import { TestingResultsContext } from '../../context/TestingResultsContext';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
-    return (
-      <header className={classes.header}>
-        <div className={classes.logo}> Testingify RestAPI </div>
-        <nav>
-          <ul>
-            <li>
-              <Link href='/'> Home </Link>
-            </li>
-            <li>
-              <Link href='/upload-oas'> Test Your API </Link>
-            </li>
-            <li>
-              <Link href='/testing-results'> Results </Link>
-            </li>
-            <li>
-              <Link href='/testing-statistics'> Statistics </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
+  const testingResultsCtx = useContext(TestingResultsContext);
+
+  const isResultsEmtpy = !!testingResultsCtx.resultsData;
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}> Testingify RestAPI </div>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/'> Home </Link>
+          </li>
+          <li>
+            <Link href='/test-api'> Test Your API </Link>
+          </li>
+          {isResultsEmtpy && (
+            <>
+              <li>
+                <Link href='/testing-results'> Results </Link>
+              </li>
+              <li>
+                <Link href='/testing-statistics'> Statistics </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default MainNavigation;
---
+++ pages/test-api.js
@@ -0,0 +1,5 @@
+const TestApiPage = () => {
+  return <h1> Test Your API </h1>;
+};
+
+export default TestApiPage;
